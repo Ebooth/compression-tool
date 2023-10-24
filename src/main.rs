84e9a65bdada309c998e4232ee3fe3efd8817b98
Ebooth <pauldejeandev@gmail.com>
2023-10-24 22:43:38 +0200

    Step1: Added code to count the frequency of characters in a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let content = read_file("tests/gutenberg.txt").expect("could not read the file");
+    let counter = count_characters_frequency(&content);
+    println!("{counter:?}");
+}
+
+fn count_characters_frequency(text: &str) -> HashMap<char, u64> {
+    let mut counter = HashMap::new();
+    text.chars().for_each(|c| {
+        *counter.entry(c).or_insert(0) += 1;
+    });
+    return counter;
+}
+
+fn read_file(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let data = fs::read_to_string(filepath)?;
+    Ok(data)
 }
